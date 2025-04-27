50861fd1ad08b7876629634a52be103d3489140b
MiraProfissional <jvitor.gmira@gmail.com>
2025-04-27 13:03:38 -0300

    :sparkles: feat: creating Auth Module, Controller and Service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ClassesModule } from './classes/classes.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, ClassesModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
