877b8ad819c373f9d53deeb5bca59ae7da603af8
MiraProfissional <jvitor.gmira@gmail.com>
2025-07-16 18:32:41 -0300

    :sparkles: feat: connecting backend to PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,30 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ClassesModule } from './classes/classes.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, ClassesModule, AuthModule],
+  imports: [
+    UsersModule,
+    ClassesModule,
+    AuthModule,
+    TypeOrmModule.forRootAsync({
+      imports: [],
+      inject: [],
+      useFactory: () => ({
+        type: 'postgres',
+        entities: [],
+
+        synchronize: true,
+
+        port: 5432,
+        username: 'postgres',
+        password: 'root',
+        host: 'postgres_db',
+        database: 'tcc-db',
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
