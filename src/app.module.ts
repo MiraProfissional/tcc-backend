bb6aeba8b1a76fda23506f0286db6faa83dbbf7c
MiraProfissional <jvitor.gmira@gmail.com>
2025-07-21 19:08:27 -0300

    :bug: fix: ajusting docker volumes and database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,7 +16,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       inject: [],
       useFactory: () => ({
         type: 'postgres',
-        entities: [],
+        autoLoadEntities: true,
 
         synchronize: true,
 
