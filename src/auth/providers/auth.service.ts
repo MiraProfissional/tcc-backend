83cd2a9d7d7e6516d09a521d7af456b5082f6796
MiraProfissional <jvitor.gmira@gmail.com>
2025-08-20 19:07:35 -0300

    :sparkles: feat: creating file to exclude pgData from CompoDoc

--- src/auth/providers/auth.service.ts
+++ src/auth/providers/auth.service.ts
@@ -9,8 +9,6 @@ export class AuthService {
   ) {}
 
   public login(email: string, password: string, id: string) {
-    const user = this.usersService.findOneById('123');
-
     return 'SAMPLE_TOKEN';
   }
 
