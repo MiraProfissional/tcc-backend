25d2f592a61d32f4f2017bd1760d0607f62d4d83
MiraProfissional <jvitor.gmira@gmail.com>
2025-09-12 13:42:20 -0300

    :recycle: refactor: ajusting Teacher and Student controller

--- src/users/controllers/students.controller.ts
+++ src/users/controllers/students.controller.ts
@@ -1,4 +1,77 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { StudentsService } from '../providers/students/students.service';
+import { GetUsersParamDto } from '../dtos/users/get-users-param.dto';
+import { CreateStudentDto } from '../dtos/students/create-student.dto';
+import { PatchUserDto } from '../dtos/users/patch-user.dto';
 
 @Controller('students')
-export class StudentsController {}
+@ApiTags('Students')
+export class StudentsController {
+  constructor(private readonly studentsService: StudentsService) {}
+
+  @Get('/{:id}')
+  @ApiOperation({
+    summary: 'Fetches a list of registered students on the application',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Users fetched succesfully based on the query',
+  })
+  @ApiQuery({
+    name: 'limit',
+    type: 'number',
+    required: false,
+    description: 'The number of entries returned per query',
+    example: 10,
+  })
+  @ApiQuery({
+    name: 'page',
+    type: 'number',
+    required: false,
+    description: 'The position of the page that you want the API to return',
+    example: 1,
+  })
+  public getStudents(@Param() getUsersParamDto?: GetUsersParamDto) {
+    console.log(getUsersParamDto);
+    return this.studentsService.findAll();
+  }
+
+  @Post()
+  @ApiOperation({
+    summary: 'Creates a student on the application',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Student created succesfully',
+  })
+  public postStudent(@Body() createStudentDto: CreateStudentDto) {
+    return this.studentsService.createStudent(createStudentDto);
+  }
+
+  @Patch()
+  public patchStudent(@Body() patchUserDto: PatchUserDto) {
+    console.log(patchUserDto);
+    return 'You sent a PATCH request';
+  }
+
+  @Delete()
+  public deleteStudent(@Query('id', ParseIntPipe) id: number) {
+    return this.studentsService.deleteStudentById(id);
+  }
+
+  @Delete('/soft-delete')
+  public softDeleteStudent(@Query('id', ParseIntPipe) id: number) {
+    return this.studentsService.softDeleteStudentById(id);
+  }
+}
--- src/users/controllers/teachers.controller.ts
+++ src/users/controllers/teachers.controller.ts
@@ -1,4 +1,77 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { TeachersService } from '../providers/teachers/teachers.service';
+import { GetUsersParamDto } from '../dtos/users/get-users-param.dto';
+import { CreateTeacherDto } from '../dtos/teachers/create-teacher.dto';
+import { ApiOperation, ApiQuery, ApiResponse } from '@nestjs/swagger';
+import { PatchUserDto } from '../dtos/users/patch-user.dto';
 
 @Controller('teachers')
-export class TeachersController {}
+export class TeachersController {
+  constructor(private readonly teachersService: TeachersService) {}
+
+  @Get('/{:id}')
+  @ApiOperation({
+    summary: 'Fetches a list of registered users on the application',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Users fetched succesfully based on the query',
+  })
+  @ApiQuery({
+    name: 'limit',
+    type: 'number',
+    required: false,
+    description: 'The number of entries returned per query',
+    example: 10,
+  })
+  @ApiQuery({
+    name: 'page',
+    type: 'number',
+    required: false,
+    description: 'The position of the page that you want the API to return',
+    example: 1,
+  })
+  public get(@Param() getUsersParamDto?: GetUsersParamDto) {
+    console.log(getUsersParamDto);
+    return this.teachersService.findAll();
+  }
+
+  @Post()
+  @ApiOperation({
+    summary: 'Creates a teacher on the application',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Teacher created succesfully',
+  })
+  @Post()
+  public postTeacher(@Body() createTeacherDto: CreateTeacherDto) {
+    return this.teachersService.createTeacher(createTeacherDto);
+  }
+
+  @Patch()
+  public patch(@Body() patchUserDto: PatchUserDto) {
+    console.log(patchUserDto);
+    return 'You sent a PATCH request';
+  }
+
+  @Delete()
+  public deleteTeacher(@Query('id', ParseIntPipe) id: number) {
+    return this.teachersService.deleteTeacherById(id);
+  }
+
+  @Delete('/soft-delete')
+  public softDeleteTeacher(@Query('id', ParseIntPipe) id: number) {
+    return this.teachersService.softDeleteTeacherById(id);
+  }
+}
